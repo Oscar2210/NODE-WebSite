5728cdcd0f8002fb8223c0ffb26ddf484e3cf4fa
Oscar Zambrano <Oscar2210@gmail.com>
2022-08-12 15:07:12 +0200

    Views, Login and register form

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,4 +10,8 @@ router.get("/about", (req, res) => res.render("about"));
 
 router.get("/contact", (req, res) => res.render("contact"));
 
+router.get("/login", (req, res) => res.render("login"));
+
+router.get("/register", (req, res) => res.render("register"));
+
 export default router;
